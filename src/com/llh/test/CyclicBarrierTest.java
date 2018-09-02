686c5a582e6fc828f1bbd9cbcf3c8744c7e083af
lihui <3053218092@qq.com>
2018-09-02 09:03:53 +0800

    cyclicBarrier的public CyclicBarrier(int parties,Runnable runnable)

---
+++ src/com/llh/test/CyclicBarrierTest.java
@@ -0,0 +1,42 @@
+package com.llh.test;
+
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: 刘莉慧
+ * Date: 2018/9/1
+ * Time: 15:14
+ * To change this template use File | Settings | File Templates.
+ */
+public class CyclicBarrierTest {
+    public static void main(String[] args) {
+        int N = 4;
+        CyclicBarrier barrier  = new CyclicBarrier(N);
+        for(int i=0;i<N;i++)
+            new Writer(barrier).start();
+    }
+    static class Writer extends Thread{
+        private CyclicBarrier cyclicBarrier;
+        public Writer(CyclicBarrier cyclicBarrier) {
+            this.cyclicBarrier = cyclicBarrier;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("线程"+Thread.currentThread().getName()+"正在写入数据...");
+            try {
+                Thread.sleep(5000);      //以睡眠来模拟写入数据操作
+                System.out.println("线程"+Thread.currentThread().getName()+"写入数据完毕，等待其他线程写入完毕");
+                cyclicBarrier.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }catch(BrokenBarrierException e){
+                e.printStackTrace();
+            }
+            System.out.println("所有线程写入完毕，继续处理其他任务...");
+        }
+    }
+}
---
+++ src/com/llh/test/CyclicBarrierTestOne.java
@@ -0,0 +1,50 @@
+package com.llh.test;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: 刘莉慧
+ * Date: 2018/9/2
+ * Time: 8:54
+ * To change this template use File | Settings | File Templates.
+ */
+//  调用CyclicBarrierTestOne的 public CyclicBarrier(int parties, Runnable barrierAction) {
+//   }   可以在线程都达到cyclicbarrier 状态后   ，执行这一个任务
+public class CyclicBarrierTestOne {
+    public static void main(String[] args) {
+        int N = 4;
+        CyclicBarrier barrier  = new CyclicBarrier(N,new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("当前线程"+Thread.currentThread().getName());
+            }
+        });
+
+        for(int i=0;i<N;i++)
+            new Writer(barrier).start();
+    }
+    static class Writer extends Thread{
+        private CyclicBarrier cyclicBarrier;
+        public Writer(CyclicBarrier cyclicBarrier) {
+            this.cyclicBarrier = cyclicBarrier;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("线程"+Thread.currentThread().getName()+"正在写入数据...");
+            try {
+                Thread.sleep(5000);      //以睡眠来模拟写入数据操作
+                System.out.println("线程"+Thread.currentThread().getName()+"写入数据完毕，等待其他线程写入完毕");
+                cyclicBarrier.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }catch(BrokenBarrierException e){
+                e.printStackTrace();
+            }
+            System.out.println("所有线程写入完毕，继续处理其他任务...");
+        }
+    }
+
+}
