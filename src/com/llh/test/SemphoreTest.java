1d55829759263f4a64e9f9ec83d6d5a2f82631d5
lihui <3053218092@qq.com>
2018-09-02 09:39:10 +0800

    Semphore 控制访问任务的线程个数

---
+++ src/com/llh/test/SemphoreTest.java
@@ -0,0 +1,42 @@
+package com.llh.test;
+
+import java.util.concurrent.Semaphore;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: 刘莉慧
+ * Date: 2018/9/2
+ * Time: 9:23
+ * To change this template use File | Settings | File Templates.
+ */
+// 控制同时访问任务的线程个数
+public class SemphoreTest {
+    public static void main(String[] args) {
+        int N = 8;            //工人数
+        Semaphore semaphore = new Semaphore(5); //机器数目
+        for(int i=0;i<N;i++)
+            new Worker(i,semaphore).start();
+    }
+
+    static class Worker extends Thread{
+        private int num;
+        private Semaphore semaphore;
+        public Worker(int num,Semaphore semaphore){
+            this.num = num;
+            this.semaphore = semaphore;
+        }
+
+        @Override
+        public void run() {
+            try {
+                semaphore.acquire();
+                System.out.println("工人"+this.num+"占用一个机器在生产...");
+                Thread.sleep(2000);
+                System.out.println("工人"+this.num+"释放出机器");
+                semaphore.release();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
